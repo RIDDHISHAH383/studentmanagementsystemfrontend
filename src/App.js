c50f3aeee6ba7d92810a1f351a6152917c6bbace
RIDDHISHAH383 <20it134@charusat.edu.in>
2022-09-14 13:45:45 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -5,30 +5,25 @@ import StudentList from './components/StudentList';
 import NavigationBar from './components/NavigationBar';
 import Footer from './components/Footer';
 import Container from 'react-bootstrap/Container';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
       <NavigationBar/>
       <Container>
       <Student/>
       <StudentList/>
+        <Routes>
+      <Route path="student" element={<Student/>}/>
+      <Route path="listStudents" element={<StudentList/>}/>
+      </Routes>
       </Container>
       <Footer/>
+      </Router>
     </div>
   );
 }
